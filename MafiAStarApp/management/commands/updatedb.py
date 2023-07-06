543af6c5a4ae824aa85ef4cc10a467e9d82e8480
8fuhst <59779937+8fuhst@users.noreply.github.com>
2023-07-06 21:26:12 +0200

    Added automated deletion of non-existant songs in DB

--- MafiAStarApp/management/commands/updatedb.py
+++ MafiAStarApp/management/commands/updatedb.py
@@ -7,7 +7,15 @@ from django.core.management import BaseCommand
 from MafiAStarApp.models import Song
 
 
+# TODO: Make paths os-independent
 class Command(BaseCommand):
+    """
+    Updates the database used by MafiAStarLite. This script automatically checks if any songs have been added to the
+    specified path, or if there are records in the MafiAStarLite-DB which have no corresponding folder in the path.
+
+    CAUTION: This script will remove all MafiAStarLite-DB records that have no folder specified in the given path -
+    meaning that all UltraStar-songs should be kept in a single folder and its subfolders!
+    """
 
     def add_arguments(self, parser):
         SONG_DIR = os.path.normpath("C:\\Program Files (x86)\\UltraStar Deluxe\\songs\\*")  # TODO: Change
@@ -63,18 +71,45 @@ class Command(BaseCommand):
             if not cover_path:
                 cover_path = "NONE"
 
-            # TODO: Add duplicate checking
-            # TODO: Update changed songs?
             song = Song(song_name=song_name, song_artist=artist, song_image_file=cover_path)
             if mp3_exists and txt_exists and not Song.objects.filter(song_name=song_name, song_artist=artist):
                 print(f"Created DB entry for: {artist} - {song_name}")
                 counters['new'] += 1
                 song.save()
-            else:
+            elif mp3_exists and txt_exists and Song.objects.filter(song_name=song_name, song_artist=artist):
                 counters['unchanged'] += 1
+        for song_from_db in Song.objects.all():
+            basepath = options['directories']
+            if basepath.endswith('*'):
+                basepath = basepath[:-2]
+            if "Windows" in platform.system():
+                if not os.path.exists(f"{basepath}\\{song_from_db.song_artist} - {song_from_db.song_name}\\") \
+                        or not os.path.exists(
+                    f"{basepath}\\{song_from_db.song_artist} - {song_from_db.song_name}\\{song_from_db.song_artist} - {song_from_db.song_name}.mp3") \
+                        or not os.path.exists(
+                    f"{basepath}\\{song_from_db.song_artist} - {song_from_db.song_name}\\{song_from_db.song_artist} - {song_from_db.song_name}.txt"):
+                    print(f"{basepath}\\{song_from_db.song_artist} - {song_from_db.song_name}\\")
+                    print(
+                        f"Deleted DB entry for: {song_from_db.song_artist} - {song_from_db.song_name}, folder, mp3 or txt missing")
+                    counters['deleted'] += 1
+                    song_from_db.delete()
+            else:
+                if not os.path.exists(f"{basepath}/{song_from_db.song_artist} - {song_from_db.song_name}/") \
+                        or not os.path.exists(
+                    f"{basepath}/{song_from_db.song_artist} - {song_from_db.song_name}/{song_from_db.song_artist} - {song_from_db.song_name}.mp3") \
+                        or not os.path.exists(
+                    f"{basepath}/{song_from_db.song_artist} - {song_from_db.song_name}/{song_from_db.song_artist} - {song_from_db.song_name}.txt"):
+                    print(f"{basepath}/{song_from_db.song_artist} - {song_from_db.song_name}/")
+                    print(
+                        f"Deleted DB entry for: {song_from_db.song_artist} - {song_from_db.song_name}, folder, mp3 or txt missing")
+                    counters['deleted'] += 1
+                    song_from_db.delete()
         amount_songs_in_ultrastar_db = counters['new'] + counters['unchanged']
         if amount_songs_in_ultrastar_db != Song.objects.count():
             print(f"Songs in Ultrastar DB: {amount_songs_in_ultrastar_db}")
             print(f"Songs in App DB: {Song.objects.count()}")
             print("Mismatch between Songs in App DB and Ultrastar DB. " +
                   "Please check the DBs and delete entries where outdated.")
+        print(f"Total songs added: {counters['new']}")
+        print(f"Total songs unchanged: {counters['unchanged']}")
+        print(f"Total songs deleted: {counters['deleted']}")
