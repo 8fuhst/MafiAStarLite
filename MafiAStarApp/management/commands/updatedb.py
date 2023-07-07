b1c8a00a448986d0566c89ea4110ff68a733e7eb
8fuhst <59779937+8fuhst@users.noreply.github.com>
2023-07-08 01:48:48 +0200

    Made updatedb paths os independant

--- MafiAStarApp/management/commands/updatedb.py
+++ MafiAStarApp/management/commands/updatedb.py
@@ -8,8 +8,6 @@ from django.core.management import BaseCommand
 from MafiAStarApp.models import Song
 
 
-# TODO: Make paths os-independent
-# TODO: Add images somehow
 class Command(BaseCommand):
     """
     Updates the database used by MafiAStarLite. This script automatically checks if any songs have been added to the
@@ -20,62 +18,51 @@ class Command(BaseCommand):
     """
 
     def add_arguments(self, parser):
-        SONG_DIR = os.path.normpath("C:\\Program Files (x86)\\UltraStar Deluxe\\songs\\*")  # TODO: Change
-        #SONG_DIR = os.path.normpath("Y:\\Karaoke-AG\\Sortierte Songs\\*")
+        SONG_DIR = os.path.normpath("C:\\Program Files (x86)\\UltraStar Deluxe\\songs\\")  # TODO: Change
+        # SONG_DIR = os.path.normpath("Y:\\Karaoke-AG\\Sortierte Songs\\*")
         parser.add_argument("--directories", metavar="folder", nargs='+', default=SONG_DIR,
                             help='The folder containing the Songs. Scans folder and recursive subfolders.')
 
     def handle(self, *args, **options):
+        PATH = options['directories']
+
         counters = {
             'new': 0,
             'unchanged': 0,
             'deleted': 0,
         }
 
-        for s in glob(options['directories']):
+        for s in glob(os.path.join(PATH, '*')):  # glob(options['directories']):
             txt_exists = False
             mp3_exists = False
-            img_count = 0
             cover_path = ""
-            if "Windows" in platform.system():
-                try:
-                    artist, song_name = s.split('\\')[-1].split(" - ")
-                except ValueError:
-                    # Catches misformatting like "Maroon V -Animals" instead of "Maroon V - Animals"
-                    if " " in s:  # Ensure that - is not used as combiner (e.g. "Karaoke-AG")
-                        lst = s.split('\\')[-1].split("-")
-                        artist = lst[0].strip()
-                        song_name = lst[1].strip()
-                    else:
-                        continue
-                if pathlib.Path(f"{s}\\").rglob("*.txt"):
-                    txt_exists = True
-                if pathlib.Path(f"{s}\\").rglob("*.mp3"):
-                    mp3_exists = True
-                for f in os.listdir(f"{s}\\"):
-                    if f.endswith(".jpg") or f.endswith(".png") or f.endswith(".jpeg"):
-                        img_count += 1
-                        if img_count == 1 or "cover" in str.lower(f):
-                            cover_path = os.path.basename(f)
-            else:
-                try:
-                    artist, song_name = s.split('/')[-1].split(" - ")
-                except ValueError:
-                    lst = s.split('/')[-1].split("-")
-                    artist = lst[0].strip()
-                    song_name = lst[1].strip()
-                if pathlib.Path(f"{s}/").rglob("*.txt"):
-                    txt_exists = True
-                if pathlib.Path(f"{s}/").rglob("*.mp3"):
-                    mp3_exists = True
-                for f in os.listdir(f"{s}/"):
-                    if f.endswith(".jpg") or f.endswith(".png") or f.endswith(".jpeg"):
-                        img_count += 1
-                        if img_count == 1 or "cover" in str.lower(f):
-                            cover_path = os.path.basename(f)
 
-            if not cover_path:
-                cover_path = "NONE"
+            try:
+                dir_name = os.path.basename(s)
+                artist, song_name = dir_name.split(" - ")
+            except ValueError:
+                # Catches misformatting like "Maroon V -Animals" instead of "Maroon V - Animals"
+                if " " in s:  # Ensure that - is not used as combiner (e.g. "Karaoke-AG")
+                    dir_name = os.path.basename(s)
+                    artist, song_name = dir_name.split("-")
+                    artist = artist.strip()
+                    song_name = song_name.strip()
+                else:  # Otherwise skip
+                    continue
+            if list(pathlib.Path(os.path.join(s)).rglob("*.txt")):  # pathlib.Path(f"{s}\\").rglob("*.txt"):
+                txt_exists = True
+            if list(pathlib.Path(os.path.join(s)).rglob("*.mp3")):
+                mp3_exists = True
+            img_paths = list(pathlib.Path(os.path.join(s)).rglob("*.jpg")) \
+                        + list(pathlib.Path(os.path.join(s)).rglob("*.png")) \
+                        + list(pathlib.Path(os.path.join(s)).rglob("*.jpeg"))
+            if img_paths:
+                cover_path = ""
+                for img in img_paths:
+                    if not cover_path:
+                        cover_path = os.path.relpath(os.path.abspath(img), start=PATH)
+                    if 'cover' in str(img):
+                        cover_path = os.path.relpath(os.path.abspath(img), start=PATH)
 
             song = Song(song_name=song_name, song_artist=artist, song_image_file=cover_path)
             if mp3_exists and txt_exists and not Song.objects.filter(song_name=song_name, song_artist=artist):
@@ -84,30 +71,18 @@ class Command(BaseCommand):
                 song.save()
             elif mp3_exists and txt_exists and Song.objects.filter(song_name=song_name, song_artist=artist):
                 counters['unchanged'] += 1
-        for song_from_db in Song.objects.all():
-            basepath = options['directories']
-            if basepath.endswith('*'):
-                basepath = basepath[:-2]
-            if "Windows" in platform.system():
-                if not os.path.exists(f"{basepath}\\{song_from_db.song_artist} - {song_from_db.song_name}\\") \
-                        or not pathlib.Path(f"{basepath}\\{song_from_db.song_artist} - {song_from_db.song_name}\\").rglob("*.mp3") \
-                        or not pathlib.Path(f"{basepath}\\{song_from_db.song_artist} - {song_from_db.song_name}\\").rglob("*.txt"):
-                        print(
-                            f"Deleted DB entry for: {song_from_db.song_artist} - {song_from_db.song_name}, folder, mp3 or txt missing")
-                        counters['deleted'] += 1
-                        song_from_db.delete()
-            else:
-                if not os.path.exists(f"{basepath}/{song_from_db.song_artist} - {song_from_db.song_name}/") \
-                        or not pathlib.Path(f"{basepath}/{song_from_db.song_artist} - {song_from_db.song_name}/").rglob("*.mp3") \
-                        or not pathlib.Path(f"{basepath}/{song_from_db.song_artist} - {song_from_db.song_name}/").rglob("*.txt"):
-                    print(f"Deleted DB entry for: {song_from_db.song_artist} - {song_from_db.song_name}, folder, mp3 or txt missing")
-                    counters['deleted'] += 1
-                    song_from_db.delete()
+        for song in Song.objects.all():
+            """if basepath.endswith('*'):
+                basepath = basepath[:-2]"""
+            if not os.path.exists(os.path.join(PATH, f"{song.song_artist} - {song.song_name}")):
+                print(f"Deleted DB entry for: {song.song_artist} - {song.song_name}, folder, mp3 or txt missing")
+                counters['deleted'] += 1
+                song.delete()
         amount_songs_in_ultrastar_db = counters['new'] + counters['unchanged']
         if amount_songs_in_ultrastar_db != Song.objects.count():
             print(f"Songs in Ultrastar DB: {amount_songs_in_ultrastar_db}")
             print(f"Songs in App DB: {Song.objects.count()}")
-            print("Mismatch between Songs in App DB and Ultrastar DB. " +
+            print("Mismatch between Songs in App DB and Ultrastar DB. "
                   "Please check the DBs and delete entries where outdated.")
         print(f"Total songs added: {counters['new']}")
         print(f"Total songs unchanged: {counters['unchanged']}")
