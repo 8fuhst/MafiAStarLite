7a75e42177e316631c81049aa7451c8b05a080c2
8fuhst <59779937+8fuhst@users.noreply.github.com>
2023-10-17 18:51:58 +0200

    Fix environment variables frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,6 @@ import App from './App.vue';
 import './index.css';
 
 const app = createApp(App);
-app.config.globalProperties.$hostname = process.env.HOSTNAME
+app.config.globalProperties.$hostname = import.meta.env.VITE_HOSTNAME
 app.mount("#app");
 
